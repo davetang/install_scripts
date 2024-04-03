69619ef4cf6909bf5504c0304b8bb998dcce1b0f
Dave Tang <davetingpongtang@gmail.com>
2024-04-03 10:13:06 +0900

    Install pigz

---
+++ scripts/pigz.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+VER=2.8
+TOOL=pigz
+URL=https://zlib.net/${TOOL}/${TOOL}-${VER}.tar.gz
+OUTDIR=${HOME}/bin/${TOOL}-${VER}
+TMPDIR=$(mktemp -d)
+
+trap "rm -rf ${TMPDIR}" SIGINT SIGTERM
+
+if [[ -d ${OUTDIR} ]]; then
+   >&2 echo ${OUTDIR} already exists
+   exit 1
+else
+   mkdir -p ${OUTDIR}
+fi
+
+cd ${TMPDIR}
+wget ${URL}
+tar -xzf ${TOOL}-${VER}.tar.gz
+cd ${TOOL}-${VER}
+make
+mv ${TOOL} un${TOOL} README ${OUTDIR}
+cd
+rm -rf ${TMPDIR}
+
+>&2 echo Done
+exit 0
