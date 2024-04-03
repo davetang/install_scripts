122098368d19e1c89088492e5f7ce112ab6ec7c3
Dave Tang <davetingpongtang@gmail.com>
2024-04-03 17:29:43 +0900

    Install Environment Modules

---
+++ scripts/modules.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+VER=5.4.0
+TOOL=modules
+URL=https://github.com/cea-hpc/modules/releases/download/v${VER}/${TOOL}-${VER}.tar.gz
+OUTDIR=${HOME}/bin/${TOOL}-${VER}
+
+read -p "This script will try to install ${TOOL} version ${VER} in ${OUTDIR}; do you want to continue? y/[n]" continue
+
+if [[ ! ${continue} == "y" ]]; then
+   >&2 echo Exiting...
+   exit 1
+fi
+
+if [[ ! -e ${HOME}/.bashrc ]]; then
+   >&2 echo ${HOME}/.bashrc not found
+   exit 1
+fi
+
+TMPDIR=$(mktemp -d)
+trap "rm -rf ${TMPDIR}" SIGINT SIGTERM
+
+if [[ -d ${OUTDIR} ]]; then
+   >&2 echo ${OUTDIR} already exists
+   exit 1
+else
+   mkdir -p ${OUTDIR}
+fi
+
+cd ${TMPDIR}
+wget ${URL}
+tar -xzf ${TOOL}-${VER}.tar.gz
+cd ${TOOL}-${VER}
+./configure --prefix=${OUTDIR}
+make && make install
+cd
+rm -rf ${TMPDIR}
+
+echo source ${OUTDIR}/init/bash >> ~/.bashrc
+
+>&2 echo Done
+exit 0
