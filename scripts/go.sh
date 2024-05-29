83e8cc70935032d22f11d6f355d9d5b0e3dc0b95
Dave Tang <davetingpongtang@gmail.com>
2024-05-29 23:56:06 +0900

    Install golang

---
+++ scripts/go.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+VER=1.22.3
+TOOL=go
+URL=https://go.dev/dl/go${VER}.linux-amd64.tar.gz
+SCRIPT_DIR=$(dirname $(realpath $0))
+source ${SCRIPT_DIR}/config.sh
+OUTDIR=${BIN}/${TOOL}-${VER}
+TMPDIR=$(mktemp -d)
+
+trap "rm -rf ${TMPDIR}" SIGINT SIGTERM
+
+source ${SCRIPT_DIR}/utils.sh
+make_install_dir ${OUTDIR}
+
+cd ${TMPDIR}
+wget ${URL} -O ${TOOL}-${VER}.tar.gz
+tar xzf ${TOOL}-${VER}.tar.gz
+mv go ${OUTDIR}
+cd
+
+rm -rf ${TMPDIR}
+
+>&2 echo Done
+exit 0
