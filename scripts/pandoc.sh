4405476dca253f94fcf373d1f4f5b2ff9edc614b
Dave Tang <davetingpongtang@gmail.com>
2025-08-22 12:35:49 +0900

    Install the universal markup converter

---
+++ scripts/pandoc.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+TOOL=pandoc
+VER=3.7.0.2
+TOOL_VER=${TOOL}-${VER}
+SCRIPT_DIR=$(dirname $(realpath $0))
+source ${SCRIPT_DIR}/config.sh
+OUTDIR=${BIN}/${TOOL}-${VER}
+TMPDIR=$(mktemp -d)
+
+trap "rm -rf ${TMPDIR}" SIGINT SIGTERM
+
+source ${SCRIPT_DIR}/utils.sh
+make_install_dir ${OUTDIR}
+
+KERNEL=$(get_kernel_name)
+ARCH=$(get_arch)
+if [[ ${KERNEL} == Linux ]]; then
+   if [[ ${ARCH} == x86_64 ]]; then
+      >&2 echo ${KERNEL} ${ARCH} detected
+      URL=https://github.com/jgm/pandoc/releases/download/${VER}/pandoc-${VER}-linux-amd64.tar.gz
+   elif [[ ${ARCH} == aarch64 ]]; then
+      >&2 echo ${KERNEL} ${ARCH} detected
+      URL=https://github.com/jgm/pandoc/releases/download/${VER}/pandoc-${VER}-linux-arm64.tar.gz
+   else
+      >&2 echo Unsupported architecture: ${ARCH}
+   fi
+elif [[ ${KERNEL} == Darwin ]]; then
+   if [[ ${ARCH} == x86_64 ]]; then
+      >&2 echo ${KERNEL} ${ARCH} detected
+      URL=https://github.com/jgm/pandoc/releases/download/${VER}/pandoc-${VER}-x86_64-macOS.zip
+   elif [[ ${ARCH} == arm64 ]]; then
+      >&2 echo ${KERNEL} ${ARCH} detected
+      URL=https://github.com/jgm/pandoc/releases/download/${VER}/pandoc-${VER}-arm64-macOS.zip
+   else
+      >&2 echo Unsupported architecture: ${ARCH}
+   fi
+else
+   >&2 echo Unsupported platform: ${KERNEL}
+fi
+
+cd ${TMPDIR}
+>&2 echo Downloading from ${URL}
+wget --quiet ${URL}
+if [[ ${KERNEL} == Linux ]]; then
+   tar xzf ${TOOL}-*.tar.gz
+   rm ${TOOL}-*.tar.gz
+elif [[ ${KERNEL} == Darwin ]]; then
+   unzip ${TOOL}-*.zip
+   rm ${TOOL}-*.zip
+fi
+cd ${TOOL}-*
+mv ./* ${OUTDIR}
+cd
+
+rm -rf ${TMPDIR}
+
+>&2 echo Done
+exit 0
