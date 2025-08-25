6aaeb90b8f6c0b1763b5337a54c98de75d52e6d4
Dave Tang <davetingpongtang@gmail.com>
2025-08-25 09:34:26 +0900

    Install luarocks

---
+++ scripts/luarocks.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+VER=3.12.2
+TOOL=luarocks
+URL=https://luarocks.org/releases/${TOOL}-${VER}.tar.gz
+SCRIPT_DIR=$(dirname $(realpath $0))
+source ${SCRIPT_DIR}/config.sh
+OUTDIR=${BIN}/${TOOL}-${VER}
+TMPDIR=$(mktemp -d)
+
+trap "rm -rf ${TMPDIR}" SIGINT SIGTERM
+
+source ${SCRIPT_DIR}/utils.sh
+make_install_dir ${OUTDIR}
+
+cd ${TMPDIR}
+wget ${URL}
+tar zxpf ${TOOL}-${VER}.tar.gz
+cd ${TOOL}-${VER}
+./configure --prefix=${OUTDIR} && make && make install
+cd
+
+rm -rf ${TMPDIR}
+
+>&2 echo Done
+exit 0
