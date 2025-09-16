3d5db08fbacfc4dbf5ff38e99269c723b4c5d140
Dave Tang <davetingpongtang@gmail.com>
2025-09-16 16:33:39 +0900

    ShellCheck and use variable for tool

--- scripts/bash_language_server.sh
+++ scripts/bash_language_server.sh
@@ -4,13 +4,15 @@ set -euo pipefail
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 source "${SCRIPT_DIR}"/utils.sh
-source ${SCRIPT_DIR}/config.sh
+source "${SCRIPT_DIR}"/config.sh
+
+MY_TOOL=bash-language-server
 
 find_tool npm
 mkdir -p "${LIB}/node_modules"
-npm install --global --prefix="${LIB}" bash-language-server
+npm install --global --prefix="${LIB}" ${MY_TOOL}
 
->&2 echo "bash-language-server installed to ${LIB}/bin/bash-language-server"
-${LIB}/bin/bash-language-server --version
+>&2 echo "${MY_TOOL} installed to ${LIB}/bin/${MY_TOOL}"
+"${LIB}"/bin/${MY_TOOL} --version
 >&2 echo Done
 exit 0
