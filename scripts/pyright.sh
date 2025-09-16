1b68906d77371aac14a28ab7ba488e1fdf739452
Dave Tang <davetingpongtang@gmail.com>
2025-09-16 16:31:33 +0900

    Install pyright locally

---
+++ scripts/pyright.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+source "${SCRIPT_DIR}"/utils.sh
+source "${SCRIPT_DIR}"/config.sh
+
+MY_TOOL=pyright
+
+find_tool npm
+mkdir -p "${LIB}/node_modules"
+npm install --global --prefix="${LIB}" ${MY_TOOL}
+
+>&2 echo "${MY_TOOL} installed to ${LIB}/bin/${MY_TOOL}"
+"${LIB}"/bin/${MY_TOOL} --version
+>&2 echo Done
+exit 0
