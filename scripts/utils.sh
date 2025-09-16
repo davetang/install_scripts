66c04b800529e0de7191afb2b686dff47336c09d
Dave Tang <davetingpongtang@gmail.com>
2025-09-16 14:36:03 +0900

    shellcheck compliant

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,14 +1,14 @@
 make_install_dir(){
-   if [[ $# < 1 ]]; then
+   if [[ $# -lt 1 ]]; then
       >&2 echo Please provide DIR path
       exit 1
    fi
    local OUTDIR=$1
    if [[ -d ${OUTDIR} ]]; then
-      >&2 echo ${OUTDIR} already exists
+      >&2 echo "${OUTDIR}" already exists
       exit 1
    else
-      mkdir -p ${OUTDIR}
+      mkdir -p "${OUTDIR}"
    fi
 }
 
@@ -24,7 +24,7 @@ get_arch(){
 
 find_tool(){
    local TOOL=$1
-   if command -v ${TOOL} > /dev/null 2>&1; then
+   if command -v "${TOOL}" > /dev/null 2>&1; then
       >&2 echo "${TOOL} found"
    else
       >&2 echo "${TOOL} is required and was not found"
