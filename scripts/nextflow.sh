4375f9594b9d6e46da1ab7ed3b8fb4e123d7d63e
Dave Tang <davetingpongtang@gmail.com>
2025-09-22 10:06:59 +0900

    Install specific version of Nextflow

--- scripts/nextflow.sh
+++ scripts/nextflow.sh
@@ -3,16 +3,25 @@
 set -euo pipefail
 
 TOOL=nextflow
+VER=25.04.3
+URL=https://github.com/nextflow-io/nextflow/releases/download/v${VER}/nextflow-${VER}-dist
 SCRIPT_DIR=$(dirname $(realpath $0))
 source ${SCRIPT_DIR}/config.sh
-OUTDIR=${BIN}
+OUTDIR=${BIN}/${TOOL}-${VER}
 TMPDIR=$(mktemp -d)
+
+make_install_dir ${OUTDIR}
+find_tool java
 
 trap "rm -rf ${TMPDIR}" SIGINT SIGTERM
 
 cd ${TMPDIR}
-curl -s https://get.nextflow.io | bash
-mv ./${TOOL} ${OUTDIR}
+>&2 echo "Downloading from ${URL}"
+wget --quiet ${URL}
+chmod 755 ./nextflow-${VER}-dist
+./nextflow-${VER}-dist -version
+mv ./nextflow-${VER}-dist ${OUTDIR}
+cd
 rm -rf ${TMPDIR}
 
 >&2 echo Done
