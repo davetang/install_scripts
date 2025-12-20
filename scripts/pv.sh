d05d0857c6f8dad4975a92aa6c4f1190b766c97e
Dave Tang <davetingpongtang@gmail.com>
2025-12-20 18:51:04 +0900

    Install Pipe Viewer

---
+++ scripts/pv.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+VER=1.10.3
+TOOL=pv
+URL=https://ivarch.com/s/pv-${VER}.tar.gz
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+source "${SCRIPT_DIR}"/config.sh
+OUTDIR=${BIN}/${TOOL}-${VER}
+TMPDIR=$(mktemp -d)
+
+trap 'rm -rf "$TMPDIR"' SIGINT SIGTERM
+
+source "${SCRIPT_DIR}"/utils.sh
+make_install_dir "${OUTDIR}"
+
+cd "${TMPDIR}"
+wget ${URL} -O ${TOOL}-${VER}.tar.gz
+tar xzf ${TOOL}-${VER}.tar.gz
+cd ${TOOL}-${VER}
+sh ./configure --prefix="${OUTDIR}"
+make && make install
+cd
+
+rm -rf "${TMPDIR}"
+
+>&2 echo Done
+exit 0
