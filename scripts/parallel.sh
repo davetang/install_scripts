e30fb80e56191833f304ea8bfce9b9b6ba712e6e
Dave Tang <davetingpongtang@gmail.com>
2026-01-14 15:59:01 +0900

    GNU parallel

---
+++ scripts/parallel.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+VER=20251222
+TOOL=parallel
+URL=https://ftp.gnu.org/gnu/parallel/parallel-${VER}.tar.bz2
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+source "${SCRIPT_DIR}"/config.sh
+OUTDIR=${BIN}/${TOOL}-${VER}
+TMPDIR=$(mktemp -d)
+
+trap 'rm -rf ${TMPDIR}' SIGINT SIGTERM
+
+source "${SCRIPT_DIR}"/utils.sh
+make_install_dir "${OUTDIR}"
+
+cd "${TMPDIR}"
+wget ${URL}
+tar xjf ${TOOL}-${VER}.tar.bz2
+cd ${TOOL}-${VER}
+./configure --prefix="${OUTDIR}"
+make && make install
+cd
+
+rm -rf "${TMPDIR}"
+
+>&2 echo Done
+exit 0
